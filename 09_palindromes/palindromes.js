ea1577d5d30fdcadf3efa3082eaf3142520f238f
hyperbanner <131620724+HyperBanner@users.noreply.github.com>
2024-06-22 15:24:10 +0200

    09-palindromes solution added

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const everything = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const omittedString = string
+    .toLowerCase()
+    .split("")
+    .filter((character) => everything.includes(character))
+    .join("");
 
+  const reversedString = omittedString.split("").reverse().join("");
+  if (omittedString === reversedString) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
