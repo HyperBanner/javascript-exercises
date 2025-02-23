05a0a089491778b215a56e73204530d9c7236554
HyperBanner <131620724+HyperBanner@users.noreply.github.com>
2025-02-23 12:04:05 +0100

    09_palindromes solution added

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+  const sanitizedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const arrStr = Array.from(sanitizedStr);
+  if (arrStr.join("") === arrStr.reverse().join("")) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
