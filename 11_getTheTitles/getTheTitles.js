ebcd86d7db27beb4eac4104c9a281ee99d730238
HyperBanner <131620724+HyperBanner@users.noreply.github.com>
2025-02-23 12:46:45 +0100

    11_getTheTitles solution added

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+  let newArray = [];
+  array.forEach((item) => {
+    for (let key in item) {
+      if (key === "title") {
+        newArray.push(item[key]);
+      }
+    }
+  });
+  return newArray;
 };
 
 // Do not edit below this line
