396829f36dc3c29b0feaca76cbcf155bbcf6c375
HyperBanner <131620724+HyperBanner@users.noreply.github.com>
2025-02-23 11:37:27 +0100

    08_calculator solutions added

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length < 1) {
+    return 0;
+  }
+  return array.reduce((total, number) => {
+    return total + number;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, number) => total * number, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1;
+  for (let i = 1; i <= a; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +38,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
