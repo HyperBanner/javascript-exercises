661e609f86a1076650cf49851b1f784fe1a522ce
HyperBanner <131620724+HyperBanner@users.noreply.github.com>
2025-02-23 13:05:09 +0100

    12_findTheOldest solution added

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const getAge = function(first, last) {
+  if (!last) {
+    last = new Date().getFullYear();
+  }
+  return last - first;
+};
 
+const findTheOldest = function(people) {
+  return people.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath,
+    );
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
 };
 
 // Do not edit below this line
